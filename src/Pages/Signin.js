f7ba750bacc7690b71c4a37a568fbcac699f2fbd
sarthak2205 <nirmal.sarthak@gmail.com>
2022-09-19 23:46:55 +0530

    Second Commit

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,43 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { RiFacebookFill } from 'react-icons/ri';
+import { BsExclamationLg } from 'react-icons/bs';
+import { useDispatch } from 'react-redux';
+import { login } from '../features/userSlice';
 
 export default function Signin() {
     const [ userID, setUserID ] = useState("");
     const [ password, setPassword ] = useState("");
-    const [ usererror, setUserError ] = useState("");
-    const [ passerror, setPassError ] = useState("");
+    const [ error, setError ] = useState({userID: "", password: ""});
 
-    const validate = () => {
-        const errors = {}
-        
-        if(password===""){
-            errors.password="Password is required!";
-        }
+    const dispatch = useDispatch(); 
 
-        if(userID===""){
-            errors.userID="Email ID or Username is required!";
-        }
-
-
-        return Object.keys(errors).length===0?null:errors;
-    }
-
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        
-        const errors = validate();
 
-        if(errors){
-            setUserError(errors.userID);
-            setPassError(errors.password);
-            console.log(setPassError);
-        }
-        else {
-            setPassError("");
-            setUserError("");
-        }
-    }
+        dispatch(
+            login({
+                userID: userID,
+                password: password,
+                loggedIn: true,
+            })
+        );
+    };
 
   return (
     <div className='w-screen h-full p-7 overflow-y-scroll'>
@@ -49,12 +34,11 @@ export default function Signin() {
             </div>
         </div>
         <div className=' h-full flex flex-col justify-center items-center  md:-mt-2'>
-            <form className='h-full flex flex-col justify-center items-center md:p-2' onSubmit={handleSubmit}>
+            <form className='h-full flex flex-col justify-center items-center md:p-2' onSubmit={(e)=>handleSubmit(e)}>
                 <div className='text-2xl md:text-3xl font-bold p-4 pb-7 pt-8 mt-2'>
                     <h1 >Log in</h1>
                 </div>
-                <div className='w-full mb-3'>
-                    <div className='rounded-2xl mb-3  bg-[#f7f7f7] focus-within:scale-110 transition duration-300 ease-in-out'>
+                <div className='w-full rounded-2xl mb-3 bg-[#f7f7f7] focus-within:scale-110 transition duration-300 ease-in-out'>
                     <input 
                         placeholder='Email or username' 
                         type="email"
@@ -63,28 +47,28 @@ export default function Signin() {
                         value={userID}
                         onChange={(e) => {
                             setUserID(e.target.value);
+                            setError({...error, userID: ""});
                         }}
-                        className= 'w-full bg-[#f7f7f7] text-gray-800 p-2 rounded-2xl border-2 border-solid border-slate-300 px-5 font-thin lg:py-4 text-base md:text-xl placeholder:font-thin placeholder:text-gray-400 password'/>
-                    </div>
-                        <div className="text-red-500 text-sm -mt-3 mb-3 ml-2">{usererror}</div>
+                        required="Required"
+                        className= 'w-full appearance-none bg-[#f7f7f7] text-gray-800 p-2 rounded-2xl border-2 border-solid border-slate-300 px-5 font-thin lg:py-4 text-base md:text-xl placeholder:font-thin placeholder:text-gray-400 password'/>{error?.userID !== "" && (
+                            <BsExclamationLg className="ml-4 h-7 text-alert" />
+                          )}
                 </div>
-                <div className='w-full mb-3'>
-                    <div className='password w-full rounded-2xl flex flex-row bg-[#f7f7f7] border-2 border-solid border-slate-300 focus-within:border-sky-400 focus-within:scale-110 transition duration-300 ease-in-out'>
-                        <input 
-                            placeholder='Password'
-                            type="password" 
-                            name='password'
-                            id='password'
-                            value={ password }
-                            onChange={(e)=>{
-                                setPassword(e.target.value);
-                            }}
-                            className= 'w-full bg-[#f7f7f7] text-gray-800 p-2 rounded-2xl px-5 lg:py-4 text-base md:text-xl placeholder:font-thin placeholder:text-gray-400 focus:outline-none'/>
-                        <div className='flex justify-center items-center text-sm font-semibold text-[#ACACAC] mr-4 '>FORGOT?</div>
-                    </div>
-                    <div className="text-red-500 text-sm ml-2">{passerror}</div>
+                <div className='password w-full rounded-2xl flex flex-row bg-[#f7f7f7] border-2 border-solid border-slate-300 focus-within:border-sky-400 focus-within:scale-110 transition duration-300 ease-in-out'>
+                    <input 
+                        placeholder='Password'
+                        type="password" 
+                        name='password'
+                        id='password'
+                        value={password}
+                        onChange={(e)=>{
+                            setPassword(e.target.value);
+                            setError({...error,password: ""});
+                        }}
+                        required
+                        className= 'w-full bg-[#f7f7f7] text-gray-800 p-2 rounded-2xl px-5 lg:py-4 text-base md:text-xl placeholder:font-thin placeholder:text-gray-400 focus:outline-none'/>
+                    <div className='flex justify-center items-center text-sm font-semibold text-[#ACACAC] mr-4 '>FORGOT?</div>
                 </div>
-                
                 <button className='w-full flex flex-row justify-center items-center bg-sky-400 font-semibold text-white text-sm lg:text-xl p-2 py-3 lg:py-3 pb-3 lg:pb-4 rounded-2xl border-b-4 border-solid border-b-sky-500 mt-5 hover:bg-sky-300 hover:scale-110 transition duration-300 ease-in-out'>LOG IN</button>
                 <div className="flex items-center py-4 lg:py-7 w-full mt-2">
                     <div className=
