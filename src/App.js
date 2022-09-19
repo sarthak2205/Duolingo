3bdf3e248bcb23c2083f36e21b3153eea4811ff0
sarthak2205 <nirmal.sarthak@gmail.com>
2022-09-20 00:12:18 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { Navbar } from './Components/Navbar';
+import { selectUser } from './features/userSlice';
 import Home from './Pages/Home';
 import Signin from './Pages/Signin';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <Router>
       <div >
-        <Routes>
-          <Route path="/" element={ <Signin/> }/>
-        </Routes>
+        {user ? <Home /> : <Routes>
+        <Route path="/" element={ <Signin/> }/>
+          </Routes>}
+          
+        
+        
       </div>
     </Router>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { RiArrowDropDownLine } from 'react-icons/ri';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logout } from '../features/userSlice';
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+
+    dispatch(logout());
+  }
+
   return (
     <div className='w-full h-25 bg-sky-800'>
         <div className='flex flex-row justify-around items-center py-4 px-1 mx-auto'>
@@ -11,6 +21,11 @@ export const Navbar = () => {
                     <img src="https://d35aaqx5ub95lt.cloudfront.net/images/dca3b978d07a7dfc05adb1d5526e9e21.svg" className='w-30 h-12'/>
             </div>
             </Link>
+            <button onClick={(e) => handleLogout(e)}>
+              <div className='text-white font-combo font-bold px-10 -mx-8'>
+                    Sign out
+              </div>
+            </button>
             <div className='text-white font-Exo font-semibold px-12 flex flex-row'>
                 <h1>SITE LANGUAGE: ENGLISH</h1> 
                 <div className='text-lg text-white'><RiArrowDropDownLine />
